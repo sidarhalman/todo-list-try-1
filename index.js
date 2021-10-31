0659f005e0a2ed6c740988c004550249dd208f4d
sidarhalman <sidarhalman93@gmail.com>
2021-10-31 16:31:42 +0100

    addtodo func

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log(window);
+const form = document.querySelector("#todo-form");
+const todoInput = document.querySelector("#todo");
+const todoList = document.querySelector(".list-group");
+const firstCardBody = document.querySelectorAll(".card-body")[0];
+const secondCardBody = document.querySelectorAll(".card-body")[1];
+const filter = document.querySelector("#filter");
+const clearButton = document.querySelector("clear-todos");
+
+
+addEventListener();
+
+function addEventListener()
+{
+    form.addEventListener("submit", addTodo);
+};
+
+function addTodo(event)
+{   
+    
+    const newTodo = todoInput.value.trim();
+    
+    console.log(newTodo);
+
+    event.preventDefault();
+}
